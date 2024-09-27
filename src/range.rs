0c9c664d4ffd37c81cfb3c73215179b98be4d38c
vrindisbacher <vrindisbacher77@gmail.com>
2024-09-26 21:17:31 -0700

    cleanup

--- src/range.rs
+++ src/range.rs
@@ -2,24 +2,6 @@
 use core::ops::Range;
 use std::iter::Step;
 
-#[flux_rs::extern_spec]
-#[flux_rs::refined_by(b: bool)]
-enum Option<T> {
-    #[variant(Option<T>[false])]
-    None,
-    #[variant({T} -> Option<T>[true])]
-    Some(T),
-}
-
-#[flux_rs::extern_spec]
-impl<T> Option<T> {
-    #[sig(fn(&Option<T>[@b]) -> bool[b])]
-    const fn is_some(&self) -> bool;
-
-    #[sig(fn(&Option<T>[@b]) -> bool[!b])]
-    const fn is_none(&self) -> bool;
-}
-
 #[flux_rs::extern_spec(core::ops)]
 #[flux_rs::refined_by(start: Idx, end: Idx)]
 struct Range<Idx> {
