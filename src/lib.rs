90bb671934e92f31074173cc7f64842df1343d83
vrindisbacher <vrindisbacher77@gmail.com>
2024-10-02 12:36:17 -0700

    Add implementation that results in postcondition cannot be proved

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,25 @@
 #![feature(step_trait)]
 
-mod option;
-mod range;
+use std::ops::Range;
+
 mod bound;
 mod into_iter;
-mod step;
 mod iterator;
+mod option;
+mod range;
+mod step;
 
-pub unsafe fn panic_begin(nop: &dyn Fn()) {
-    // Let any outstanding uart DMA's finish
-    for _ in 0..200000 {
-        nop();
+#[flux_rs::refined_by(internal_len: int, start: int, end: int)]
+pub struct MyStruct<'a, T> {
+    #[field({ &[T][internal_len] | internal_len > 0 })]
+    internal: &'a [T],
+    #[field({ Range<usize>[start, end] | start >= 0 && start < end && end < internal_len })]
+    range: Range<usize>,
+}
+
+impl<'a, T> MyStruct<'a, T> {
+    #[flux_rs::sig(fn(&MyStruct<T>[@internal_len, @start, @end]) -> &[T][end - start])]
+    fn slice_me_up(&'a self) -> &'a [T] {
+        &self.internal[self.range.start..self.range.end]
     }
 }
