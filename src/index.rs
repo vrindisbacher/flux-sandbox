16d39df4850fd93cc825e21c6348962392dd1741
vrindisbacher <vrindisbacher77@gmail.com>
2024-10-03 11:42:06 -0700

    Progress

--- src/index.rs
+++ src/index.rs
@@ -1,9 +1,9 @@
 #![allow(unused)]
-use std::slice::SliceIndex;
 use std::ops::Range;
+use std::slice::SliceIndex;
 
 #[flux_rs::extern_spec(std::ops)]
-#[generics(Idx as base)]
+// #[generics(Idx as base)]
 trait Index<Idx> {
     #[flux_rs::sig(fn(&Self, Idx) -> &Self::Output)]
     fn index(&self, index: Idx) -> &Self::Output;
@@ -11,22 +11,24 @@ trait Index<Idx> {
 
 #[flux_rs::extern_spec(std::ops)]
 #[generics(I as base)]
-impl<T, I: SliceIndex<[T]>> Index<I> for [T] { 
+impl<T, I: SliceIndex<[T]>> Index<I> for [T] {
     // problem here: How can I refine `I`???
-    #[flux_rs::sig(fn(&[T][@slice], idx: I) -> &I::Output::indexed(slice, idx))]
-    fn index(&self, index: I) -> &I::Output;
+    #[flux::sig(fn (&[T][@len], {I[@idx] | <I as SliceIndex<[T]>>::in_bounds(idx, len)}) -> _)]
+    fn index(&self, index: I) -> &<I as SliceIndex<[T]>>::Output;
 }
 
 #[flux_rs::extern_spec(std::slice)]
-#[generics(T as base)]
-#[flux_rs::assoc(fn indexed(slice: T, idx: Self) -> int)]
+#[flux::generics(Self as base, T as base)]
+#[flux::assoc(fn in_bounds(idx: Self, v: T) -> bool)]
 trait SliceIndex<T> {
     #[flux_rs::sig(fn(Self, &T) -> &Self::Output)]
-    fn index(self, slice: &T) -> &Self::Output where Self: Sized;
+    fn index(self, slice: &T) -> &Self::Output
+    where
+        Self: Sized;
 }
 
 #[flux_rs::extern_spec(std::slice)]
-#[flux_rs::assoc(fn indexed(slice: int, idx: Range<int>) -> int { idx.end - idx.start })]
+#[flux::assoc(fn in_bounds(idx: Range<int>, len: int) -> bool { idx.start >= 0 && idx.start <= idx.end && idx.end < len } )]
 impl<T> SliceIndex<[T]> for Range<usize> {
     #[flux_rs::sig(fn(Range<usize>[@start, @end], &[T][@len]) -> &[T][end - start])]
     fn index(self, slice: &[T]) -> &[T];
