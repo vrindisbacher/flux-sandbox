4e7556503c7b0c749e862ed6fdb570ea9258d3b9
vrindisbacher <vrindisbacher77@gmail.com>
2024-10-03 12:41:17 -0700

    Add extern specs for Index & IndexMut on slices

--- src/index.rs
+++ src/index.rs
@@ -3,23 +3,20 @@ use std::ops::Range;
 use std::slice::SliceIndex;
 
 #[flux_rs::extern_spec(std::ops)]
-// #[generics(Idx as base)]
 trait Index<Idx> {
     #[flux_rs::sig(fn(&Self, Idx) -> &Self::Output)]
     fn index(&self, index: Idx) -> &Self::Output;
 }
 
 #[flux_rs::extern_spec(std::ops)]
-#[generics(I as base)]
-impl<T, I: SliceIndex<[T]>> Index<I> for [T] {
-    #[flux::sig(fn (&[T][@len], {I[@idx] | <I as SliceIndex<[T]>>::in_bounds(idx, len)}) -> I {o: <I as SliceIndex<[T]>>::indexed(idx, o) })]
-    fn index(&self, index: I) -> &<I as SliceIndex<[T]>>::Output;
+trait IndexMut<Idx> {
+    #[flux_rs::sig(fn(&mut Self, Idx) -> &mut Self::Output)]
+    fn index_mut(&mut self, index: Idx) -> &mut Self::Output;
 }
 
 #[flux_rs::extern_spec(std::slice)]
 #[flux::generics(Self as base, T as base)]
 #[flux::assoc(fn in_bounds(idx: Self, v: T) -> bool)]
-#[flux::assoc(fn indexed(idx: Self, v: T) -> bool)]
 trait SliceIndex<T> {
     #[flux_rs::sig(fn(Self, &T) -> &Self::Output)]
     fn index(self, slice: &T) -> &Self::Output
@@ -27,9 +24,22 @@ trait SliceIndex<T> {
         Self: Sized;
 }
 
+#[flux_rs::extern_spec(std::ops)]
+#[generics(I as base)]
+impl<T, I: SliceIndex<[T]>> Index<I> for [T] {
+    #[flux::sig(fn (&[T][@len], {I[@idx] | <I as SliceIndex<[T]>>::in_bounds(idx, len)}) -> _)]
+    fn index(&self, index: I) -> &<I as SliceIndex<[T]>>::Output;
+}
+
+#[flux_rs::extern_spec(std::ops)]
+#[flux_rs::generics(I as base)]
+impl<T, I: SliceIndex<[T]>> IndexMut<I> for [T] {
+    #[flux::sig(fn (&mut [T][@len], {I[@idx] | <I as SliceIndex<[T]>>::in_bounds(idx, len)}) -> _)]
+    fn index_mut(&mut self, idx: I) -> &mut <I as SliceIndex<[T]>>::Output;
+}
+
 #[flux_rs::extern_spec(std::slice)]
 #[flux::assoc(fn in_bounds(idx: Range<int>, len: int) -> bool { idx.start >= 0 && idx.start <= idx.end && idx.end < len } )]
-#[flux::assoc(fn indexed(idx: Range<int>, len: int) -> bool { len == idx.end - idx.start })]
 impl<T> SliceIndex<[T]> for Range<usize> {
     #[flux_rs::sig(fn(Range<usize>[@start, @end], &[T][@len]) -> &[T][end - start])]
     fn index(self, slice: &[T]) -> &[T];
--- src/lib.rs
+++ src/lib.rs
@@ -19,9 +19,24 @@ pub struct MyStruct<'a, T> {
     range: Range<usize>,
 }
 
+#[flux_rs::refined_by(internal_len: int, start: int, end: int)]
+pub struct MyStructMut<'a, T> {
+    #[field({ &mut [T][internal_len] | internal_len > 0 })]
+    internal: &'a mut [T],
+    #[field({ Range<usize>[start, end] | start >= 0 && start < end && end < internal_len })]
+    range: Range<usize>,
+}
+
 impl<'a, T> MyStruct<'a, T> {
-    #[flux_rs::sig(fn(&MyStruct<T>[@internal_len, @start, @end]) -> &[T][end - start])]
+    #[flux_rs::sig(fn(&MyStruct<T>[@internal_len, @start, @end]) -> &[T])]
     fn slice_me_up(&'a self) -> &'a [T] {
         &self.internal[self.range.start..self.range.end]
+    }
+}
+
+impl<'a, T> MyStructMut<'a, T> {
+    #[flux_rs::sig(fn(&mut MyStructMut<T>[@internal_len, @start, @end]) -> &[T])]
+    fn slice_me_up(&'a mut self) -> &'a [T] {
+        &mut self.internal[self.range.start..self.range.end]
     }
 }
