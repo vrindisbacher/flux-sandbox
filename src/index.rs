ef182d2cdd7f36106499bd8c2e6724afc47a5f2f
vrindisbacher <vrindisbacher77@gmail.com>
2024-10-02 17:32:59 -0700

    Most basic impl of Index extern spec possible

--- src/index.rs
+++ src/index.rs
@@ -1,15 +1,28 @@
 #![allow(unused)]
 use std::slice::SliceIndex;
+use std::ops::Range;
 
-// #[flux_rs::extern_spec(std::ops)]
-// #[flux_rs::assoc(fn slice_index_resulting_len(inp: Self:Output,  res: Self::Output, Self) -> bool)]
-// trait SliceIndex<T> {
-//     #[flux_rs::sig(fn(Self, &T) -> &Self::Output)]
-//     fn index(self, slice: &T) -> &Self::Output;
-// }
+#[flux_rs::extern_spec(std::ops)]
+#[generics(Idx as base)]
+trait Index<Idx> {
+    #[flux_rs::sig(fn(&Self, Idx) -> &Self::Output)]
+    fn index(&self, index: Idx) -> &Self::Output;
+}
 
 #[flux_rs::extern_spec(std::ops)]
 #[generics(T as base)]
+impl<T, I: SliceIndex<[T]>> Index<I> for [T] { 
+    #[flux_rs::sig(fn(&[T], I) -> &I::Output)]
+    fn index(&self, index: I) -> &I::Output;
+}
+
+#[flux_rs::extern_spec(std::slice)]
+trait SliceIndex<T> {
+    #[flux_rs::sig(fn(Self, &T) -> &Self::Output)]
+    fn index(self, slice: &T) -> &Self::Output where Self: Sized;
+}
+
+#[flux_rs::extern_spec(std::slice)]
 impl<T> SliceIndex<[T]> for Range<usize> {
     #[flux_rs::sig(fn(Range<usize>[@start, @end], &[T][@len]) -> &[T][end - start])]
     fn index(self, slice: &[T]) -> &[T];
